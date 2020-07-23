df0dfd3179756f6b6ca13b476022ee69a18dbb42
solomencev <68597538+solomencev@users.noreply.github.com>
2020-07-23 12:43:12 +0300

    Update test_main_page.py

--- test_main_page.py
+++ test_main_page.py
@@ -14,40 +14,38 @@ def test_add_contacts():
     exit_from_profile(browser)
 
 
-def exit_from_profile(browser):
-    my_account = browser.find_element_by_xpath('//*[@id="top-links"]/ul/li[2]/a/span[1]').click()
-    logout = browser.find_element_by_xpath('//*[@id="top-links"]/ul/li[2]/ul/li[5]/a').click()
-
-
-def create_contact(browser):
-    # create new contact
-    submit = browser.find_element_by_xpath('//*[@id="content"]/form/div/div[2]/input').click()
-
-
-def fill_required_fields(browser):
-    first = browser.find_element_by_xpath('//*[@id="input-firstname"]').send_keys('Alexsandr')
-    last = browser.find_element_by_xpath('//*[@id="input-lastname"]').send_keys('Ivanov')
-    adress1 = browser.find_element_by_xpath('//*[@id="input-address-1"]').send_keys('Moscow, Lenina streer, house 8')
-    city = browser.find_element_by_xpath('//*[@id="input-city"]').send_keys('Moscow')
-    post_code = browser.find_element_by_xpath('//*[@id="input-postcode"]').send_keys(152121)
-    select = Select(browser.find_element_by_xpath('//*[@id="input-country"]'))
-    select.select_by_value('176')
-    time.sleep(1)
-    select = Select(browser.find_element_by_xpath('//*[@id="input-zone"]'))
-    select.select_by_value('2761')
-
-
-def open_book_contact(browser):
-    book = browser.find_element_by_xpath('//*[@id="column-right"]/div/a[4]').click()
-    new_adress = browser.find_element_by_xpath('//*[@id="content"]/div/div[2]/a').click()
-
-
-def login(browser):
-    email = browser.find_element_by_xpath('//*[@id="input-email"]').send_keys('exjfizhh@firste.ml')
-    password = browser.find_element_by_xpath('//*[@id="input-password"]').send_keys('admin')
-    submit = browser.find_element_by_xpath('//*[@id="content"]/div/div[2]/div/form/input').click()
-
-
 def open_page(wd):
     wd.get('https://demo.opencart.com/index.php?route=account/login')
 
+
+def login(wd):
+    wd.find_element_by_xpath('//*[@id="input-email"]').send_keys('exjfizhh@firste.ml')
+    wd.find_element_by_xpath('//*[@id="input-password"]').send_keys('admin')
+    wd.find_element_by_xpath('//*[@id="content"]/div/div[2]/div/form/input').click()
+
+
+def open_book_contact(wd):
+    wd.find_element_by_xpath('//*[@id="column-right"]/div/a[4]').click()
+    wd.find_element_by_xpath('//*[@id="content"]/div/div[2]/a').click()
+
+
+def fill_required_fields(wd):
+    wd.find_element_by_xpath('//*[@id="input-firstname"]').send_keys('Alexsandr')
+    wd.find_element_by_xpath('//*[@id="input-lastname"]').send_keys('Ivanov')
+    wd.find_element_by_xpath('//*[@id="input-address-1"]').send_keys('Moscow, Lenina streer, house 8')
+    wd.find_element_by_xpath('//*[@id="input-city"]').send_keys('Moscow')
+    wd.find_element_by_xpath('//*[@id="input-postcode"]').send_keys(152121)
+    select = Select(wd.find_element_by_xpath('//*[@id="input-country"]'))
+    select.select_by_value('176')
+    time.sleep(1)
+    select = Select(wd.find_element_by_xpath('//*[@id="input-zone"]'))
+    select.select_by_value('2761')
+
+
+def create_contact(wd):
+    wd.find_element_by_xpath('//*[@id="content"]/form/div/div[2]/input').click()
+
+
+def exit_from_profile(wd):
+    wd.find_element_by_xpath('//*[@id="top-links"]/ul/li[2]/a/span[1]').click()
+    wd.find_element_by_xpath('//*[@id="top-links"]/ul/li[2]/ul/li[5]/a').click()
