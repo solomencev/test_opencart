019a9a1e79924dbfb12d89211b9dfafb359ec153
solomencev <68597538+solomencev@users.noreply.github.com>
2020-07-23 17:19:51 +0300

    Update test_main_page.py

--- test_main_page.py
+++ test_main_page.py
@@ -15,6 +15,7 @@ class test_open_cart(unittest.TestCase):
         wd.maximize_window()
 
     def login(self, wd, user_name, password):
+        self.open_page(wd)
         wd.find_element_by_xpath('//*[@id="input-email"]').send_keys(user_name)
         wd.find_element_by_xpath('//*[@id="input-password"]').send_keys(password)
         wd.find_element_by_xpath('//*[@id="content"]/div/div[2]/div/form/input').click()
@@ -24,6 +25,7 @@ class test_open_cart(unittest.TestCase):
         wd.find_element_by_xpath('//*[@id="content"]/div/div[2]/a').click()
 
     def fill_required_fields(self, wd, contact):
+        self.open_book_contact(wd)
         wd.find_element_by_xpath('//*[@id="input-firstname"]').send_keys(contact.first_name)
         wd.find_element_by_xpath('//*[@id="input-lastname"]').send_keys(contact.last_name)
         wd.find_element_by_xpath('//*[@id="input-address-1"]').send_keys(contact.adress)
@@ -34,7 +36,8 @@ class test_open_cart(unittest.TestCase):
         time.sleep(1)
         select = Select(wd.find_element_by_xpath('//*[@id="input-zone"]'))
         select.select_by_value(contact.zone_in_country)
-
+        self.create_contact(wd)
+        
     def create_contact(self, wd):
         wd.find_element_by_xpath('//*[@id="content"]/form/div/div[2]/input').click()
 
@@ -45,13 +48,11 @@ class test_open_cart(unittest.TestCase):
 
     def test_add_contacts(self):
         wd = self.wd
-        self.open_page(wd)
         self.login(wd, user_name='exjfizhh@firste.ml', password='admin')
-        self.open_book_contact(wd)
         self.fill_required_fields(wd, Contact(first_name='Alexsandr', last_name='Ivanov',
                                   adress='Moscow, Lenina streer, house 8', city='Moscow', post_code=152121,
                                   value_of_country='176', zone_in_country='2761'))
-        self.create_contact(wd)
+
         self.exit_from_profile(wd)
 
     def tearDown(self):
